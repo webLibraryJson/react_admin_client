ae697f2c383f3e3985e878a3e9e507d940e98a35
JSON_CHENG <2276129351@qq.com>
2021-08-31 14:17:21 +0800

    react后端管理系统

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    welcom:"hello React"
+  }
+  change = (welcom) =>{
+    this.setState({welcom})
+  }
+  changeTow = (params) =>{
+    return () => {
+      this.setState({welcom:params})
+    }
+  }
+  render() {
+    return (
+      <div>
+        {this.state.welcom}
+        <button onClick={() =>{this.change("你好 react")}}>更换titleOne</button>
+        <button onClick={this.changeTow('hello react')}>更换titleTow</button>
+      </div>
+      
+    )
+  }
 }
 
-export default App;
