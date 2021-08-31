29fef46458104d897d3503c6ad112541e826c660
JSON_CHENG <2276129351@qq.com>
2021-08-31 21:25:15 +0800

    自定义主题+按需加载

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import {Button,message,Input} from 'antd'
+// import 'antd/dist/antd.css';
+// import 'antd/dist/antd.less';
 
 export default class App extends Component {
+  sumbit = () =>{
+    message.success('提交成功');
+  }
   render() {
     return (
       <div>
-      
+        <Input placeholder="Basic usage" />
+        <Button type="primary" onClick={this.sumbit}>提交</Button>
       </div>
       
     )
