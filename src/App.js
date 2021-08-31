b95124e58ebdee6d8c7589accab2118b726508f7
JSON_CHENG <2276129351@qq.com>
2021-08-31 14:56:01 +0800

    文件解构搭建

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
-  state = {
-    welcom:"hello React"
-  }
-  change = (welcom) =>{
-    this.setState({welcom})
-  }
-  changeTow = (params) =>{
-    return () => {
-      this.setState({welcom:params})
-    }
-  }
   render() {
     return (
       <div>
-        {this.state.welcom}
-        <button onClick={() =>{this.change("你好 react")}}>更换titleOne</button>
-        <button onClick={this.changeTow('hello react')}>更换titleTow</button>
+      
       </div>
       
     )
